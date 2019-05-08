3e99f6aac418192e6dbce401fe3c2092e3db10a0
alk831 <adam54111@gmail.com>
2019-05-08 12:34:21 +0200

    build: update

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,37 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const defaultOptions = {
     autoEmit: true,
-    emitName: '$'
+    emitPrefix: '$',
+    listenActions: {}
 };
 const middleware = (options) => {
+    /* Setup listeners and send dispatch argument on first (connect) event */
     const mergedOptions = {
         ...defaultOptions,
         ...options
     };
-    return (store) => (next) => (action) => {
-        const { socket } = mergedOptions;
-        next(action);
-        if (action.meta && action.meta.io === true) {
-            socket.emit(action.type, action, store.dispatch);
+    const { socket, listenActions } = mergedOptions;
+    if (socket == null) {
+        throw new Error(`You have not passed socket instance to middleware options`);
+    }
+    return (store) => {
+        for (let actionType in listenActions) {
+            socket.on(actionType, (action) => {
+                store.dispatch({
+                    ...action,
+                    meta: {
+                        io: false,
+                        ...action.meta || {}
+                    }
+                });
+            });
         }
+        return (next) => (action) => {
+            next(action);
+            if (action.meta && action.meta.io === true) {
+                socket.emit(action.type, action, store.dispatch);
+            }
+        };
     };
 };
 exports.default = middleware;
